9c841d3362a71922f6f9971b08345c50fb61b380
ZachCodedThat <Zachemailsback@gmail.com>
2021-08-21 14:52:06 -0400

    added docs

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,11 +4,9 @@ import matter from "gray-matter";
 import marked from "marked";
 import Link from "next/link";
 
-const PostPage = ({
-  frontmatter: { title, date, cover_image },
-  slug,
-  content,
-}) => {
+const PostPage = ({ frontmatter: { title, date, cover_image }, content }) => {
+  // frontmatter and content are props from getStaticProps.
+
   return (
     <>
       <Link href="/">
@@ -32,13 +30,20 @@ const PostPage = ({
 export async function getStaticPaths() {
   const files = fs.readdirSync(path.join("posts"));
 
+  // creates a variable that creates an array of all of the files within the posts directory
+
   const paths = files.map((filename) => ({
     params: { slug: filename.replace(".md", "") },
   }));
 
+  // takes the variable "files" and maps it against a function that for every file in the posts folder that removes the .md tag from the filename
+  // this creates an [{}] which is sent to getStaticProps when a new slug is called for.
+
   return {
     paths,
+    // this returns params: { slug: filename.replace(".md", "") }
     fallback: false,
+    // this throws a 404 if an unknown path/slug is called
   };
 }
 
@@ -48,13 +53,20 @@ export async function getStaticProps({ params: { slug } }) {
     "utf-8"
   );
 
+  // getStaticProps takes in chosen slug within the "posts" directory passed from getStaticPaths
+  // creates a variable that is content of the markdown file within the posts directory created by the slug + .md
+  // utf-8 refers to the HTML character set
+
   const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  // matter takes the markdown file and parses it to seperate the front-matter and the content needing to be rendered.
+  // the parsed file returns a props {} containing data from the parsed elemtents that can now be used with the JSX above to render content to the page.
 
   return {
     props: {
       frontmatter,
-      slug,
       content,
+      slug,
     },
   };
 }
