c9162aaa868c8ff788aed7f8d9c50d9118ea7b86
ZachCodedThat <Zachemailsback@gmail.com>
2021-08-27 00:50:39 -0400

    hid env variables

--- src/utils/initSupabase.js
+++ src/utils/initSupabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wcsuwyjfxnvkdjvdjcxz.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDAyNjQyNywiZXhwIjoxOTQ1NjAyNDI3fQ.5Phixi0164uGy0U0vj9uPjeXln5B7OAEmBl4eZDRsek";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
