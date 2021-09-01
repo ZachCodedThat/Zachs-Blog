44ccfba523f895ab25f5ab59863dd505ef739dea
ZachCodedThat <Zachemailsback@gmail.com>
2021-09-01 12:17:05 -0400

    Added image to home page only

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,19 @@ import path from "path";
 import Post from "@components/Post";
 import { sortByDate } from "@utils/index";
 import Container from "@components/Container";
+import { Image } from "@chakra-ui/react";
 
 const Home = ({ posts }) => {
+  const randomNumber = Math.floor(Math.random() * 100);
   return (
     <>
       <Container>
+        <Image
+          src={`https://avatars.dicebear.com/api/bottts/${randomNumber}.svg`}
+          margin-top="1rem"
+          boxSize="3rem"
+          position="top"
+        />
         <div>
           {posts.map((post, index) => (
             <Post post={post} key={index} />
