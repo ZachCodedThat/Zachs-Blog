1b17ead3c2f3742f51b1aa378794d287f2e2f2f2
ZachCodedThat <Zachemailsback@gmail.com>
2021-10-27 11:20:02 -0400

    more missing imports

--- src/components/404.js
+++ src/components/404.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Box, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useColorMode } from "@chakra-ui/react";
-import { textColor } from "../utils/colors";
+import { textColor } from "@styles/colorModeStyles";
 const NotFound = () => {
   const router = useRouter();
   const { colorMode } = useColorMode();
